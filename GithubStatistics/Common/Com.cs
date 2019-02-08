050409dc6e298592f36319887b61a5e94c03cadf
Wolix <Rak.wojtek@wp.pl>
2019-02-08 21:36:53 +0100

    Add Cache for Token

---
+++ GithubStatistics/Common/Com.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubStatistics.Common
+{
+    public static class Com
+    {
+        public const string AccesToken = "AccesToken";
+    }
+}
--- GithubStatistics/Views/SettingsPage.xaml.cs
+++ GithubStatistics/Views/SettingsPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using GithubStatistics.Common;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,14 +24,26 @@ namespace GithubStatistics.Views
     /// </summary>
     public sealed partial class SettingsPage : Page
     {
+        private readonly ApplicationDataContainer _applicationDataContainer;
         public SettingsPage()
         {
             this.InitializeComponent();
+            _applicationDataContainer = ApplicationData.Current.LocalSettings;
+            InitializeToken();
+        }
+
+        private void InitializeToken()
+        {
+            var value = _applicationDataContainer.Values[Com.AccesToken];
+            if (value != null)
+            {
+                Token.Text = value.ToString();
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            _applicationDataContainer.Values[Com.AccesToken] = Token.Text;
         }
     }
 }
