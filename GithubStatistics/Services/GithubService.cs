caecf8d7211b4c87a2f8bfdf8f012998e62423f3
Wolix <Rak.wojtek@wp.pl>
2019-02-08 22:17:14 +0100

    Update GithubService.cs

--- GithubStatistics/Services/GithubService.cs
+++ GithubStatistics/Services/GithubService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using GithubStatistics.Interfaces;
@@ -9,6 +10,13 @@ namespace GithubStatistics.Services
 {
     public class GithubService : IGithubService
     {
+        private static readonly HttpClient client = new HttpClient();
+        public string SearchUser()
+        {
+            var responseString = client.GetStringAsync("http://www.example.com/recepticle.aspx");
+            return responseString.Result;
+        }
+
         public int Test()
         {
             return 1;
